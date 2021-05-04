1c292b8a45bd5c4df4df68e3f5fd21cb67e7cf05
rafael <rafaelsanto10s@gmail.com>
2021-05-04 20:28:45 -0300

    listando documentos boards do firebase

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,26 +1,56 @@
+import { useEffect, useState } from 'react';
 import Board from '../../components/Board';
 import Input from '../../components/Input';
 import Button from '@material-ui/core/Button';
 import ModalComponent from '../../components/ModalComponent';
 
+import firebase from '../../services/firebaseConnection';
+
 import './style.css'
 
 export default function Home() {
+  const [boards, setBoards] = useState([]);
+
+  useEffect(() => {
+    try {
+      (
+        async () => {
+          await firebase.firestore().collection('Boards')
+          .get()
+          .then((snapshot)=> {
+            const list = [];
+
+            snapshot.forEach((doc)=> {
+              list.push(doc.data().Title)
+            })
+    
+            setBoards(list)
+    
+          })
+        }
+
+      )()
+    } catch (error) {
+      console.log('Error:', error )
+    }
+  }, [])
+
+
   return (
     <>
       <div className="containerBoards">
         <h2>Meus quadros</h2>
         <div className="boards">
-          <Board name="Projeto 1" />
-          <Board name="Projeto 2" />
-          <Board name="Projeto 3" />
+          {boards.map((item, index) => (
+            <Board key={index} name={item} />
+          ))}
         </div>
       </div>
 
       <ModalComponent>
         <div className="containerNewBoard">
           <h3>Novo quadro</h3>
-          <form action="">
+          <form>
             <div className="controllInput">
               <Input label="Nome" />
             </div>
